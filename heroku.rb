eda0e50e36b0a9504a1782a61bcc8694fee6654a
Michael Edlund <medlund@mac.com>
2018-01-18 14:20:06 -0800

    Use file

--- heroku.rb
+++ heroku.rb
@@ -11,15 +11,19 @@ gem_group :staging, :production do
 end
 
 run "echo \"\nruby '2.4.3'\n\n\" >> Gemfile"
-run "echo web:\ bundle\ exec\ puma\ -C\ config/puma.rb\nworker:\ rake\ jobs:work\n > Procfile"
 
-env = %Q{ 
-  DEVELOPMENT_HOSTNAME=mysql.website.com
-  DEVELOPMENT_DB_NAME=website_development
-  DEVELOPMENT_USERNAME=user
-  DEVELOPMENT_PASSWORD=pass
-  RAILS_ENV=development
-  RACK_ENV=development
-  SECRET_KEY_BASE=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-}
-run "\"#{env}\" > .env"
+file 'Procfile', <<-CODE
+web: bundle exec puma -C config/puma.rb
+worker: rake jobs:work
+CODE
+
+file '.env', <<-CODE
+DEVELOPMENT_HOSTNAME=mysql.website.com
+DEVELOPMENT_DB_NAME=website_development
+DEVELOPMENT_USERNAME=user
+DEVELOPMENT_PASSWORD=pass
+RAILS_ENV=development
+RACK_ENV=development
+SECRET_KEY_BASE=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+CODE
+
